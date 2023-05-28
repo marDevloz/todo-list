105a42a4b76fc3ea5ecb1ba44c68b64293f5a915
Manuel Valdez <mrvaloz@gmail.com>
2023-05-27 21:55:44 -0500

    configurado el entorno para comenzar a trabajar en la app lista de taread

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MiComponenteFuncional() {
+    return(
+        <div>
+            <h1>Soy un componente funcional</h1>
+        </div>
+    );
+    
+}
+
+export default MiComponenteFuncional;
